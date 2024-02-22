424f9f5bb56e7d0be525da083cc16e7f21a1a6c5
Abigail Cherdy Hartono <abigailchartono@gmail.com>
2024-02-22 22:19:25 +0800

    second commit Module 2 Assignment

--- script.js
+++ script.js
@@ -7,3 +7,27 @@ hamburger.addEventListener("click", () => {
 }
 )
 
+function dark() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+
+    if (element.classList.contains("dark-mode")) {
+   //if class was added to body
+        localStorage.setItem("background", "dark-mode");
+   //save information in "background" localStorage variable, use dark-mode class name
+    } else {
+        localStorage.setItem("background", "");
+  //if class was removed set background" localStorage variable to null
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+//when loading document
+    var background = localStorage.getItem("background");
+  // get localStorage var background
+    if (background) {
+  // if its not null and empty
+        document.body.className += background;
+  //add class to body
+    }
+});
